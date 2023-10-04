a410892e9a0a6a283ba11567a07978a517db043f
bigh0124 <thubgi0629@gmail.com>
2023-10-04 15:38:12 +0800

    Feat: 連結至mongo database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongodb");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const app = express();
 
 app.listen(3000, () => {
+  connect();
   console.log("Server is running on port 3000");
 });
