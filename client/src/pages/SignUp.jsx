0e2b4fb54378a5d0c3a30a18c15050638605bf8b
bigh0124 <thubgi0629@gmail.com>
2023-10-05 14:56:03 +0800

    Fix: 修復註冊頁面漏洞

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -24,6 +24,7 @@ const SignUp = () => {
       if (data.success === false) {
         setIsLoading(false);
         setError(data.message);
+        return;
       }
       setIsLoading(false);
       navigate("/");
