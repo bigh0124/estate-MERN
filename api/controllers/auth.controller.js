b14a63ebf93ffc6cb5084e64f4536f561f831bd4
bigh0124 <thubgi0629@gmail.com>
2023-10-05 16:29:31 +0800

    Feat: 新增登入api與cookie

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { createError } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   try {
@@ -19,5 +19,20 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User has been created");
   } catch (err) {
     next(createError(err));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(createError(404, "Wrong credentials"));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(createError(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...info } = validUser._doc;
+    res.cookie("access_token", token, { httpOnly: true }, { maxAge: 86400000 }).status(200).json(info);
+  } catch (err) {
+    next(err);
   }
 };
