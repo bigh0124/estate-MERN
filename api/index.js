41ba9f0cb94976020a1de889340bec8735f5b278
bigh0124 <thubgi0629@gmail.com>
2023-10-05 14:30:25 +0800

    Fix. 解決CORS問題

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
@@ -16,6 +17,12 @@ const connect = async () => {
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
