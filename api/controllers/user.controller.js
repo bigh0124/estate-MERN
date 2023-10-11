7266531ea08d39c3be875af9857fd9932c6a9891
bigh0124 <thubgi0629@gmail.com>
2023-10-11 09:36:19 +0800

    Feat: 新增update user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,31 @@
+import { createError } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 export const test = (req, res) => {
   res.send("test");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) return next(createError(401, "You can only update your own profile"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...info } = updatedUser._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    next(err);
+  }
 };
