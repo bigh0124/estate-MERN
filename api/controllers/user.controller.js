d486531594f3981404b096ad8cfa44e01ab04f80
bigh0124 <thubgi0629@gmail.com>
2023-10-11 16:49:19 +0800

    Feat: 新增delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,3 +29,13 @@ export const updateUser = async (req, res, next) => {
     next(err);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) return next(createError(401, "You can only delete your own account"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).clearCookie("access_token").send("Account has been deleted!");
+  } catch (err) {
+    next(err);
+  }
+};
