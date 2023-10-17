0aafbfd8ad645e9504161f1096e304938751d5af
bigh0124 <thubgi0629@gmail.com>
2023-10-17 15:08:07 +0800

    Feat: 新增update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -20,3 +20,15 @@ export const deleteListing = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(createError(404, "listing not found"));
+    if (listing.userRef !== req.user.id) return next(createError(403, "You can only update your own listing"));
+    const newListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(newListing);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verify from "../middleware/verifyToken.js";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 const router = express.Router();
 
 router.post("/create", verify, createListing);
 router.delete("/delete/:id", verify, deleteListing);
+router.put("/update/:id", verify, updateListing);
 
 export default router;
