39ad3f1d946692156562ab12bcde84e9a9981025
bigh0124 <thubgi0629@gmail.com>
2023-10-17 13:30:24 +0800

    Feat: 新增delete listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,21 @@
 import Listing from "../models/listing.model.js";
+import { createError } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
     const newListing = await Listing.create(req.body);
     res.status(201).json(newListing);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (listing.userRef !== req.user.id) return next(createError(403, "You can only delete your own listing"));
+    await listing.deleteOne();
+    res.status(200).json("Listing has been deleted!");
   } catch (err) {
     next(err);
   }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import verify from "../middleware/verifyToken.js";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing } from "../controllers/listing.controller.js";
 const router = express.Router();
 
 router.post("/create", verify, createListing);
+router.delete("/delete/:id", verify, deleteListing);
 
 export default router;
