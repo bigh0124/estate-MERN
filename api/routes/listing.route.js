4ca5a6adb096c6a035854f86284b3153a3b31656
bigh0124 <thubgi0629@gmail.com>
2023-10-19 08:27:17 +0800

    Feat: 新增 search api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,18 @@
 import express from "express";
 import verify from "../middleware/verifyToken.js";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+  getListing,
+  getListings,
+  updateListing,
+} from "../controllers/listing.controller.js";
 const router = express.Router();
 
 router.post("/create", verify, createListing);
 router.delete("/delete/:id", verify, deleteListing);
 router.put("/update/:id", verify, updateListing);
 router.get("/fetchListing/:id", getListing);
+router.get("/fetchListings", getListings);
+
 export default router;
