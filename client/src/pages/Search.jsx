f6910c0a7c1016b532982a303ae3e072f0ea2c6f
bigh0124 <thubgi0629@gmail.com>
2023-10-19 10:33:37 +0800

    Feat: 新增 listingItem UI

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import newRequest from "../utils/request";
+import ListingItem from "../components/ListingItem";
 
 const Search = () => {
   const navigate = useNavigate();
@@ -191,6 +192,21 @@ const Search = () => {
           </div>
           <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">Search</button>
         </form>
+      </div>
+      <div className="flex-1">
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">Listing results:</h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && <p className="text-xl text-slate-700">No listing found!</p>}
+          {loading && <p className="text-xl text-slate-700 text-center w-full">Loading...</p>}
+
+          {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing={listing} />)}
+
+          {showMore && (
+            <button onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">
+              Show more
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
