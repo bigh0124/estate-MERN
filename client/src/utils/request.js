e74c0d60d44a6fc04f7087344183aefd77a071fc
bigh0124 <thubgi0629@gmail.com>
2023-10-23 13:33:42 +0800

    Fix: 修正deploy cors 問題

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "http://localhost:3000/api/",
-  withCredentials: true,
+  withCredentials: false,
 });
 
 export default newRequest;
