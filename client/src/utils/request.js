6263f77c088c6457159d1624925a59663bf4c6f6
bigh0124 <thubgi0629@gmail.com>
2023-10-23 14:35:53 +0800

    Fix: 修正env name error

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const string = import.meta.env.PORT || "http://localhost:3000/api/";
+const url = import.meta.env.VITE_PORT || "http://localhost:3000/api/";
 
 const newRequest = axios.create({
-  baseURL: string,
+  baseURL: url,
   withCredentials: false,
 });
 
